8b51e3666c8bb10ff5ca222387ceef21e4487de9
Lucas Felix <81514103+strFelix@users.noreply.github.com>
2023-10-30 19:28:38 -0300

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MVVM_API_SampleProject.Views;
+
+namespace MVVM_API_SampleProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new PostView();
+        }
+    }
+}
---
+++ ViewModels/PostViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MVVM_API_SampleProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_API_SampleProject.ViewModels
+{
+    public class PostViewModel
+    {
+        public Post Post {  get; set; }
+
+        public PostViewModel()
+        {
+            Post = new Post()
+            {
+                UserId = 1,
+                Id = 1,
+                Title = "Titulo",
+                Body = "Texto"
+            };
+        }
+    }
+}
